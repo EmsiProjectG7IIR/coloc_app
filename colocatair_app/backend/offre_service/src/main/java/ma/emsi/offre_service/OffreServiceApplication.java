770052ae78362ee6f07163fee44177b1378bd4cd
GateCoding <meriammeri.mm@gmail.com>
2023-12-23 18:45:14 +0100

    setup server and offre

---
+++ colocatair_app/backend/offre_service/src/main/java/ma/emsi/offre_service/OffreServiceApplication.java
@@ -0,0 +1,15 @@
+package ma.emsi.offre_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OffreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OffreServiceApplication.class, args);
+    }
+
+}
