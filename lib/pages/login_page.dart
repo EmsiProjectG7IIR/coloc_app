c904f7910330b3ca875da5c9958b5803337f5673
YahyaMallouli <ymallouli02@gmail.com>
2024-01-16 22:53:16 +0100

    BY

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -13,7 +13,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // text editing controllers
+  
   final emailController = TextEditingController();
 
   final passwordController = TextEditingController();
